8dd1fad53d4042d6e4fbf41bedd25518df3c8b1d
Mark Godwin <godwin.mark@gmail.com>
2023-08-28 15:04:41 -0600

    Graphs are working again, time selector hookup

--- app/controllers/rails_observatory/controller_metrics_controller.rb
+++ app/controllers/rails_observatory/controller_metrics_controller.rb
@@ -1,17 +1,24 @@
 module RailsObservatory
   class ControllerMetricsController < ApplicationController
     def index
-      @time_frame = (1.hour.ago..)
+      @time_range = (duration.seconds.ago..)
+      puts "time_range: #{@time_range} #{duration} #{params[:duration]}"
       @all_series = TimeSeries.where(name: 'process_action.action_controller.count', action: '*', method: nil, format: nil, status: nil)
-      @all_series = @all_series.select { |series| series.last_timestamp > @time_frame.begin.to_i }
-      @request_count_range = TimeSeries.where(name: "process_action.action_controller.count", action: nil).first[@time_frame].rollup(buckets: 80)
-      @latency_range = TimeSeries.where(name:"process_action.action_controller.latency", action:nil).first[@time_frame].rollup(buckets: 120)
-      @controller_metrics = ControllerMetric.in_time_frame(@time_frame)
+      @all_series = @all_series.select { |series| series.last_timestamp > @time_range.begin.to_i }
+      @request_count_range = TimeSeries.where(name: "process_action.action_controller.count", action: nil, method: nil, format: nil, status: nil).first[@time_range].rollup(buckets: 80)
+      @latency_range = TimeSeries.where(name:"process_action.action_controller.latency", action:nil, method: nil, format: nil, status: nil).first[@time_range].rollup(buckets: 120)
+      @controller_metrics = ControllerMetric.in_time_frame(@time_range)
     end
 
     def show
-      @time_frame = (1.hour.ago..)
+      @time_range = (1.hour.ago..)
       @controller_metric = ControllerMetric.find(params[:id])
+    end
+
+    private
+
+    def duration
+      (params[:duration].presence || 1.hour).to_i
     end
   end
 end
